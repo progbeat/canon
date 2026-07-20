803acc5f7949b63408a133ef0d9b4d6b392d424a
Roman Rizvanov <progbeat@gmail.com>
2026-07-20 21:29:19 +0200

    Update canon

--- .canon/specs/evaluate.py
+++ .canon/specs/evaluate.py
@@ -111,4 +111,4 @@ class _ShellEvaluator(_Evaluator):
         print(f'Command exited with code {self.response.answer} (expected {self.expected}).')
 
 
-_STATUS_TO_STR = {PASS: 'ok', FAIL: 'fail'}
+_STATUS_TO_STR = {PASS: 'OK', FAIL: 'FAIL'}
